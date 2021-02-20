5e79e3d2d0ad0df4e04d115bbb853ab084a7a11a
Cole Epperson <caepperson@gmail.com>
2021-02-20 02:38:58 +0000

    syntax and formatting

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,7 +8,7 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = ["Who was the first American woman in space? ", "True or false: 5000 meters = 5 kilometers. ", "(5+2)/2*10=? ", "Given the array[8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let questions = ["Who was the first American woman in space? ", "True or false: 5000 meters == 5 kilometers. ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, \"Orbit\", \"Trajectory\", 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
@@ -45,9 +45,9 @@ let numCorrect = 0
   let grade = (numCorrect / questions.length * 100);
   console.log(`>>> Overall Grade: ${grade}% (${numCorrect} of ${questions.length} correct) <<<`)
 if (grade >= 80){
-  console.log(`>>> Status: Pass <<<`)
+  console.log(`>>> Status: PASSED <<<`)
 } else {
-  console.log(`>>> Status: Failed <<<`)
+  console.log(`>>> Status: FAILED <<<`)
 }
 
   return grade;
